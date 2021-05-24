faa229c48ff8a3a9d71a74d018ba9af9713b4980
Victor M <victomc.586@gmail.com>
2021-05-23 21:41:25 -0500

    error handling on javaScript

---
+++ Functions/TryAndCatch.js
@@ -0,0 +1,29 @@
+/**
+ * We use try-catch blocks when handling exception errors thrown by the application is
+ * required.
+ */
+
+const person = {
+  firstName: "victor",
+  lastName: "Mora",
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set fullName(value) {
+    if (typeof value !== "string") throw new Error("value is not a string");
+
+    const parts = value.split(" ");
+    if (parts.length != 2) throw new Error("Enter a firs and a last name");
+
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  },
+};
+
+person.fullName = null;
+
+try {
+  console.log(person);
+} catch (e) {
+  alert(e);
+}
