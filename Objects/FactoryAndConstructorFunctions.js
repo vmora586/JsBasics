6433ea5b2149d00ec1d6bfa1f8cec7ae61308d4c
Victor M <victomc.586@gmail.com>
2021-06-05 19:28:50 -0500

    added documentation for advicing when using Factory and Constructor ways for creating objects (in case the object have at least one method in its properties

--- Objects/FactoryAndConstructorFunctions.js
+++ Objects/FactoryAndConstructorFunctions.js
@@ -1,6 +1,8 @@
 /**
  * In the previous excercise we used the object literal sintax for initializing an object. Now try
  * to do the same by using the factory and then the constructor function.
+ * NOTE: When an object contains at least on method in its prperties, we say this object has behaviour.
+ * For those cases, it is hihgly recommended use factory or constructor instead of object literal sintax.
  */
 
 let address = createAddress("a", "b", "c");
