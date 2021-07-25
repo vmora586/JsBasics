1e013339ea7ca4c78745feb1b3c3b3e0aba8223f
Victor M <victomc.586@gmail.com>
2021-07-25 00:40:42 -0500

    introduction to the ES6 classes. Working with the class declaration sintax and first steps with methods declaration and constructors

---
+++ ES6 Classes/ClassesIntroduction.js
@@ -0,0 +1,35 @@
+/**
+ * There are two different ways for declaring classes on ES6 (2015 JS). By using class declaration or
+ * by using Class initialization (to assign the class to a variable.) By convention and familiarity,
+ * where are going to work with class the class declaration approach.
+ */
+
+//we must use key word class and the class name using Upper case
+
+//How we would do this on Prototypical Inheritance
+
+// function Circle(radius){
+//     this.radius=radius;
+
+//     this.draw=function(){
+//         console.log("draw");
+//     }
+// }
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+
+    this.greeting = function () {
+      //methods added inside constructor belong to this specific object.
+      console.log("greetings");
+    };
+  }
+
+  draw() {
+    console.log("draw"); //at difference on Prototypical Inheritance, all methods added on class body, belongs
+    //to its prototype (can be inherited for any child object)
+  }
+}
+
+const c = new Circle(1);
